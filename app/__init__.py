66fbf98367e581a37f3fe4c5149055bdc447b216
Cody Myers <codymyers63@gmail.com>
2017-11-08 15:53:58 +0000

    Added basic directory structure.

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+'''
+This file is called by "from app import app" inside the app.py file. 
+
+It includes all the imports to be used in the app (from allImports import *).
+It also includes all the application files that are used as "pages" in the app
+(e.g., "from app import start" imports all the code in start.py that is behind the start.html webpage)
+'''
+
+from allImports import *
+from app import allImports
+from app.controllers import *
