fe084b82242807ca7d8cf18ed3895f158564d977
Jesson Soto Ventura <jesson@sotoventura.com>
2018-01-17 10:59:33 -0500

    Draft for Gallery Submissions page

--- app/controllers/gallery_submission.py
+++ app/controllers/gallery_submission.py
@@ -1,10 +1,15 @@
-from app.allImports import *
+from app.allImports import * 
 from app.logic.validation import *
 from werkzeug.security import check_password_hash
+from app.logic.queries import GalleryQueries
 from flask import session
 
-@app.route('/gallery/submission', methods=["GET","POST"])
-def gallery_submission():
+@app.route('/gallery/submission/<int:gid>', methods=["GET","POST"])
+def gallery_submission(gid):
+
+    gallery = GalleryQueries.get(gid)
+    import pdb;pdb.set_trace()
+
     return render_template('views/gallery_submission.html')
 
 
---
+++ app/logic/queries/GalleryQueries.py
@@ -0,0 +1,16 @@
+from app.models.Galleries import *
+def get(gid):
+    """ Retrieves a single gallery object
+
+    Args:
+        gid (int): The gid of the gallery model to retrieve 
+
+    Returns:
+        Gallery: The gallery object if it exists
+        None: If the gallery object does not exist
+    """
+
+    if type(gid) is int:
+        if Galleries.select().where(Galleries.gid == gid).exists():
+            return Galleries.get(Galleries.gid == gid)
+    return None
