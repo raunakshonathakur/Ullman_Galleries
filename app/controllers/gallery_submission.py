b71c1fb4ff99fd87e28fc97db3fee64b8514c1e3
Jesson Soto Ventura <jesson@sotoventura.com>
2018-01-19 15:26:08 -0500

    Finished first draft, missing copy to clipboard

--- app/controllers/gallery_submission.py
+++ app/controllers/gallery_submission.py
@@ -7,8 +7,10 @@ from flask import session
 @app.route('/gallery/submission/<int:gid>', methods=["GET","POST"])
 def gallery_submission(gid):
     gallery = GalleryQueries.get(gid)
-    print(getUsernameFromEnv())
-    return render_template('views/gallery_submission.html', gallery=gallery)
+    is_admin = False
+    if(doesUserHaveRole('admin')):
+        is_admin = True
+    return render_template('views/gallery_submission.html', gallery=gallery, is_admin=is_admin)
 
 
 
